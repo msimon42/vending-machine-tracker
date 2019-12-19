fe5c402f0fa3fc9b39700cdcf02f04ceebfce33c
msimon42 <msimon@uab.edu>
2019-12-19 10:02:31 -0700

    Add tests for snack show

---
+++ spec/features/snack_show_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe 'when I visit a snack show page', type: :feature do
+  before :each do
+    @owner = Owner.create(name: "Sam's Snacks")
+    @machine_1 = @owner.machines.create(location: "Don's Mixed Drinks")
+    @machine_2 = @owner.machines.create(location: "Turing Basement")
+
+    @snack_1 = @machine_1.snacks.create(name: 'Chocolate Delight', price: 50.00)
+    @snack_2 = @machine_1.snacks.create(name: 'Candy Surprise', price: 25.00)
+    @snack_3 = @machine_2.snacks.create(name: 'Mr. Waffle', price: 28.00)
+    @machine_2.snacks << @snack_1
+
+    visit "/snacks/#{@snack_1.id}"
+  end
+
+  it 'can see snack info' do
+    expect(page).to have_content(@snack_1.name)
+    expect(page).to have_content(@snack_1.price)
+  end
+
+  it 'can see machine info' do
+    expect(page).to have_content("#{@machine_1.name} (#{@machine_1.snack_count} kinds of snacks, avg price: #{@machine_1.avg_price})")
+    expect(page).to have_content("#{@machine_2.name} (#{@machine_2.snack_count} kinds of snacks, avg price: #{@machine_2.avg_price})")
+  end
+end
