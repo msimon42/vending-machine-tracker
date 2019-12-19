2c1612d1ba49fbc6e88fe9add603db6013e748e3
msimon42 <msimon@uab.edu>
2019-12-19 10:27:51 -0700

    Add tests for relationships

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
+
+    it {should have_many :snack_machines}
+    it {should have_many(:snacks).through(:snack_machines)}
   end
 
   describe 'methods' do
--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Snack, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :price }
+
+    it {should have_many :snack_machines}
+    it {should have_many(:machines).through(:snack_machines)}
+  end
 end
