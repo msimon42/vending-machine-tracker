b687b3f653b8f25091a6f61685f83a7768a9a113
msimon42 <msimon@uab.edu>
2019-12-19 10:18:41 -0700

    Add helper method

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -1,5 +1,12 @@
 class SnacksController < ApplicationController
   def show
     @snack = Snack.find(params[:id])
+    machines(@snack)
+  end
+
+  private
+
+  def machines(snack)
+    @machines = snack.machines
   end
 end
